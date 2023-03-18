af780ed7e888e42b13be6b0b61ee38ecc7c8c718
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-18 12:37:56 +0000

    Hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints and that piece of art is useful...'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fwrite(str, sizeof(str), 1, stdout);
+return (1);
+}
