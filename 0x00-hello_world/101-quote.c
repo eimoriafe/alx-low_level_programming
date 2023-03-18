d31a86bdd2b88995c0c6a7a043ad88de3948e893
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-18 14:31:01 +0000

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
 i * Description: 'Prints and that piece of art is useful...'
@@ -6,7 +7,8 @@ i * Description: 'Prints and that piece of art is useful...'
  */
 int main(void)
 {
-char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fwrite(str, sizeof(str), 1, stdout);
+/*char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";*/
+/* fwrite(str, sizeof(str), , stdout);*/
+write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
