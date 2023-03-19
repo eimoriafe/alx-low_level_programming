54a7321d768494ec6ae48213a160ff18f81b3a00
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-19 16:00:11 +0000

    Hello world

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: to print numbers, followed by a new line
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+char aph[] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+i = strlen(aph);
+for (i = 26; i >= 0; i--)
+{
+putchar(aph[i]);
+
+}
+putchar('\n');
+return (0);
+}
