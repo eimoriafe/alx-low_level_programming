6a04c1affca2288be04ca71fa761222dec7b8265
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-22 15:44:34 +0000

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
- * main - check the code.
- * Description: 'To print alphabets in lower case'
- * Return: 0 Always success
+ * print_alphabet- To print alphabets in lower case
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
