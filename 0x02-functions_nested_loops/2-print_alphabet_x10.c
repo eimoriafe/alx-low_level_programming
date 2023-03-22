c11d5d05ffeb6948175fe467224b8bce56718659
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-22 18:33:03 +0000

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 char n;
 int c;
 c = 0;
-	while (c < 10)
-	{	for (n = 'a'; n <= 'z'; n++)
-		{	
-			_putchar(n);
-		}
-		_putchar('\n');
-	c++;
-	}
+while (c < 10)
+{
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
 }
-
+_putchar('\n');
+c++;
+}
+}
