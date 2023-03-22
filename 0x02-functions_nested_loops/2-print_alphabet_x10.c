12e73504d817617903fd173cd2b003c29ca732a3
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-22 18:28:53 +0000

    functions

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - To print alphabets 10 times in lower case
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+char n;
+int c;
+c = 0;
+	while (c < 10)
+	{	for (n = 'a'; n <= 'z'; n++)
+		{	
+			_putchar(n);
+		}
+		_putchar('\n');
+	c++;
+	}
+}
+
