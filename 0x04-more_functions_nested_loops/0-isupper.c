dd4f15f4b47192cae3503e60b7267250c518b8bb
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-23 18:36:02 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - checking for upper case
+* @c : character that is checked.
+* Return: 1 or 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
