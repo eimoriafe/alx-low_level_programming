dfdedba818a1b147a87e77fdab2fc3e850ae0091
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-23 23:30:53 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - prints excluding 2 and 4
+* Return: 0
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+if (!(i == 2 || i == 4))
+{
+_putchar('0' + i);
+}
+}
+_putchar('\n');
+}
