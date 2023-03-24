0bb63f2bab1aee805713fbd78b0561b905e27b1f
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-24 00:43:38 +0000

    more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* more_numbers - prints excluding 2 and 4
+* Return: 0
+*/
+void more_numbers(void)
+{
+int i;
+int line_count;
+for (line_count = 0; line_count < 10; line_count++)
+{ 
+	for (i = 0; i < 15; i++)
+	{
+		if (i > 9)
+		{
+		_putchar((i / 10) + '0');
+		}
+		else
+		{
+		_putchar((i % 10) + '0');
+		}
+	}
+	_putchar('\n');
+}
+_putchar('\n');
+}
