26e0f3e20de4e4ae67348f775eb611e1d78f0d1a
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-03-24 02:01:13 +0000

    functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,11 @@
  */
 int print_last_digit(int n)
 {
-	int ldigit;
-	ldigit = n % 10;
-
-	if (ldigit < 0)
-		ldigit *= -1;
-
+int ldigit;
+ldigit = n % 10;
+if (ldigit < 0)
+{
+	ldigit *= -1;
 	_putchar(ldigit + '0');
-
 	return (ldigit);
 }
