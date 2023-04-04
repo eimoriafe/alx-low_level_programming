04e939ca33de61503fdc7441671cc5db2d2c406d
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-04-04 04:58:50 +0000

    More pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,7 +10,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
-
+void set_string(char **s, char *to);
 /**void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
