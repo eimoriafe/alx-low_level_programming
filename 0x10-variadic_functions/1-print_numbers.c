b1c1164e5de51e65e46090ffe7a6ca270dea7ee0
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-04-20 18:13:05 +0100

    variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print numbers, followed by a new line.
+ * @separator: string to be printed numbers.
+ * @n: integers to be passed to the function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator || (separator && !i))
+			printf("%d", va_arg(args, int));
+		else
+			printf("%s%d", separator, va_arg(args, int));
+	}
+	va_end(args);
+	printf("\n");
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints all
+ * @format: types of arguments passed to the function
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	char *s, *sep = "";
+	int i = 0;
+	va_list args_list;
+
+	va_start(args_list, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args_list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args_list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args_list, double));
+					break;
+				case 's':
+					s = va_arg(args_list, char *);
+					if (!s)
+						s = "(nil)";
+					printf("%s%s", sep, s);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args_list);
+}
