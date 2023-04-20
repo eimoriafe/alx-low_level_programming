121633005c17d836f7636d84e8316075f031ef13
Emmanuel Imoriafe <e.imoriafe@outlook.com>
2023-04-20 19:54:43 +0100

    function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,18 +5,18 @@
  * @array: integer array.
  * @size: array size
  * @cmp: pointer to function.
- * Return: index 
+ * Return: index
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
-	if (array && cmp)
-	{
-		if (size <= 0)
-			return (-1);
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
-	}
-	return (-1);
+int i;
+if (array && cmp)
+{
+if (size <= 0)
+return (-1);
+for (i = 0; i < size; i++)
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
 }
